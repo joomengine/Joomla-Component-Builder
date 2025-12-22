2a62a9131e003c94abdf953c37f0299810a7d94d
Antonio Scuotto <scuottolinx@gmail.com>
2025-12-22 08:48:23 +0000

    Fix: Add use GuidHelper in compiler

--- libraries/vendor_jcb/VDM.Joomla/src/Componentbuilder/Compiler/Creator/FieldXML.php
+++ libraries/vendor_jcb/VDM.Joomla/src/Componentbuilder/Compiler/Creator/FieldXML.php
@@ -28,6 +28,7 @@ use VDM\Joomla\Utilities\StringHelper;
 use VDM\Joomla\Utilities\String\FieldHelper;
 use VDM\Joomla\Utilities\ArrayHelper;
 use VDM\Joomla\Utilities\ObjectHelper;
+use VDM\Joomla\Utilities\GuidHelper;
 use VDM\Joomla\Componentbuilder\Compiler\Utilities\Line;
 use VDM\Joomla\Componentbuilder\Compiler\Interfaces\Creator\Fieldtypeinterface;
 
